bbef26ada439d866fa8aa2ad9459b5cc2908e0d1
Mario-Marchio <mariomarchio1610@gmail.com>
2023-09-12 15:16:15 +0200

    added api but doesnt work

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,25 @@
 
 <script>
+import axios from 'axios';
+const endpoint = 'http://127.0.0.1:8000/api/posts/';
 export default {
-
+  data: () => ({ posts: [] }),
+  methods: {
+    fetchPost() {
+      axios.get(endpoint).then(res => { console.log(res.data) })
+    }
+  },
+  created() {
+    this.fetchPost();
+  },
 };
 </script>
 
 <template>
   <main class="container my-3">
-
-    <h1>ciao</h1>
+    <ul>
+      <li v-for="post in posts" :key="post.id">{{ post.title }}</li>
+    </ul>
   </main>
 </template>
 
